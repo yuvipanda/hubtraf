764f191f33d9cc60fe347563c0ec8f378213879b
yuvipanda <yuvipanda@gmail.com>
2018-07-28 00:27:07 -0700

    Refactor hubtraf to be cleaner
    - Split up __main__ to multiple files
    - Use generic functions to do authentication. This allows a lot of
      flexibility without requiring heavyweight class based machinery.

---
+++ hubtraf/auth/dummy.py
@@ -0,0 +1,22 @@
+import time
+from hubtraf.user import OperationError
+
+
+async def login_dummy(session, hub_url, log, username, password):
+    """
+    Log in username with password to hub_url in aiohttp session.
+
+    log is used to emit timing and status information.
+    """
+    start_time = time.monotonic()
+
+    url = hub_url / 'hub/login'
+
+    try:
+        resp = await session.post(url, data={'username': username, 'password': password}, allow_redirects=False)
+    except Exception as e:
+        log.msg('Login: Failed with exception {}'.format(repr(e)), action='login', phase='failed', duration=time.monotonic() - start_time)
+        raise OperationError()
+    if resp.status != 302:
+        log.msg('Login: Failed with response {}'.format(str(resp)), action='login', phase='failed', duration=time.monotonic() - start_time)
+        raise OperationError()
---
+++ hubtraf/auth/lti.py
@@ -0,0 +1,42 @@
+from oauthlib.oauth1.rfc5849 import signature
+import time
+import uuid
+from hubtraf.user import OperationError
+
+
+async def lti_login_data(session, log, hub_url, username, consumer_key, consumer_secret, launch_url, extra_args={}):
+    """
+    Log in username with LTI info to hub_url
+
+    log is used to emit timing and status information.
+    """
+    args = {
+        'oauth_consumer_key': consumer_key,
+        'oauth_timestamp': str(time.time()),
+        'oauth_nonce': str(uuid.uuid4()),
+        'user_id': username
+    }
+
+    args.update(extra_args)
+
+    base_string = signature.construct_base_string(
+        'POST',
+        signature.normalize_base_string_uri(launch_url),
+        signature.normalize_parameters(
+            signature.collect_parameters(body=args, headers={})
+        )
+    )
+
+    args['oauth_signature'] = signature.sign_hmac_sha1(base_string, consumer_secret, None)
+
+    url = hub_url / 'lti/launch'
+
+    try:
+        resp = await session.post(url, data=args, allow_redirects=False)
+    except Exception as e:
+        log.msg('Login: Failed with exception {}'.format(repr(e)), action='login', phase='failed', duration=time.monotonic() - start_time)
+        raise OperationError()
+    if resp.status != 302:
+        log.msg('Login: Failed with response {}'.format(str(resp)), action='login', phase='failed', duration=time.monotonic() - start_time)
+        raise OperationError()
+    return args
